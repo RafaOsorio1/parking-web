8c4bf50e2685f8612392b8560f1ea7e2f25e6e48
Rafael Rodelo <94405449+RafaOsorio1@users.noreply.github.com>
2026-09-02 12:37:20 -0500

    feat: human-readable error formatting in web client

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -1,4 +1,4 @@
-import ky from 'ky';
+import ky, { HTTPError } from 'ky';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -6,5 +6,23 @@ export const httpClient = ky.create({
   prefix: API_URL,
   headers: {
     'Content-Type': 'application/json',
+  },
+  hooks: {
+    beforeError: [
+      async (error: HTTPError) => {
+        const { response } = error;
+        if (response) {
+          try {
+            const body = (await response.clone().json()) as { message?: string };
+            if (body && body.message) {
+              error.message = body.message;
+            }
+          } catch {
+            // Fallback if response body is not JSON
+          }
+        }
+        return error;
+      },
+    ] as any,
   },
 });
